73694a8cac93a145aeccb8af4ff6f9e842548779
mista_k <mistakster@gmail.com>
2011-05-28 18:25:49 +0600

    опции модуля

---
+++ app-options.js
@@ -0,0 +1,18 @@
+(function () {
+
+    var options = {};
+
+    APP.namespace("Options", {
+
+        get: function (module) {
+
+        },
+
+        set: function (module, obj) {
+
+        }
+
+    });
+
+
+}());

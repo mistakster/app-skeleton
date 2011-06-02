b4b37ec2a223cd43cd7b71ef4e04601f858ac5b9
mista_k <mista_k@z-inc.net>
2011-06-02 10:53:10 +0600

    рефакторинг вычисления зависимостей

--- app-dependency.js
+++ app-dependency.js
@@ -5,13 +5,18 @@
      *      name: имя
      *      path: путь до загружаемого файла
      *      requires: список модулей, которые должны быть загружены до него
+     *      skip: уже загружен или не требует загрузки
      */
 
-    var D = {
+    function isArray(obj) {
+        return Object.prototype.toString.apply(obj) === "[object Array]";
+    }
+
+    App.namespace("App.Dependency", {
         /**
          * Список зарегистрированных модулей
          */
-        _dependencies: {},
+        _storage: {},
 
         /**
          * Добавляем список зависимостей модуля в приложение
@@ -19,18 +24,19 @@
          */
         add: function (module) {
 
+            var D = this, i;
+
             function register(m) {
                 if (m && m.name) {
-                    D._dependencies[m.name] = {
+                    D._storage[m.name] = {
                         path: m.path || "",
-                        requires: m.requires || []
+                        requires: m.requires || [],
+                        skip: m.skip || false
                     };
                 }
             }
 
-            var i;
-
-            if (App.Lang.isArray(module)) {
+            if (isArray(module)) {
                 for (i = module.length; i--;) {
                     register(module[i]);
                 }
@@ -40,13 +46,16 @@
         },
 
         _collect: function (name) {
-            var UNSHIFT = Array.prototype.unshift,
-                module = D._dependencies[name],
+            var D = this,
+                UNSHIFT = Array.prototype.unshift,
+                module = D._storage[name],
                 sequence = [],
                 i;
 
-            if (module) {
-                sequence.push(module.path);
+            if (name && module) {
+                if (module.path && !module.skip) {
+                    sequence.push(name);
+                }
                 if (module.requires.length > 0) {
                     for (i = module.requires.length; i--;) {
                         UNSHIFT.apply(sequence, D._collect(module.requires[i]));
@@ -57,17 +66,15 @@
             return sequence;
         },
 
-        _reduce: function (original) {
-            var reduced = [],
-                cache = {},
-                i,
-                path;
+        _reduce: function (original, skip) {
+            var obj, i, name, reduced = [], cache = {}, D = this;
 
             for (i = 0; i < original.length; i++) {
-                path = original[i];
-                if (path && !cache[path]) {
-                    cache[path] = true;
-                    reduced.push(path);
+                name = original[i];
+                if (name && !cache[name]) {
+                    obj = D._storage[name];
+                    cache[name] = reduced.push(obj.path);
+                    obj.skip = obj.skip || skip;
                 }
             }
 
@@ -76,14 +83,14 @@
 
         /**
          * Вычисляем список зависимостей для указанных модулей
-         * @param target    {String|Array}
-         * @return          {Array}
+         * @param target    {String|Array}  один или несколько целевых модулей
+         * @param keep      {Boolean}       не отмечам использованные модули
+         * @return          {Array}         список адресов для загрузки
          */
-        calculate: function (target) {
-            var PUSH = Array.prototype.push,
-                sequence, i;
+        calculate: function (target, keep) {
+            var i, sequence, PUSH = Array.prototype.push, D = this;
 
-            if (target && Object.prototype.toString.call(target) === "[object Array]") {
+            if (isArray(target)) {
                 sequence = [];
                 for (i = 0; i < target.length; i++) {
                     PUSH.apply(sequence, D._collect(target[i]));
@@ -92,10 +99,8 @@
                 sequence = D._collect(target);
             }
 
-            return D._reduce(sequence);
+            return D._reduce(sequence, !keep);
         }
-    };
-
-    App.namespace("Dependency", D);
+    });
 
 }());
